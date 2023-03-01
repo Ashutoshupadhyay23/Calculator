f814a599a3c4159e8c4ae08220e2587698cfc009
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2023-03-01 16:16:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// TODO: Make M+, M- and MC functional
+
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    }
+    //AC Logic
+    else if (e.target.innerHTML == "AC") {
+      string = "";
+      document.querySelector("input").value = string;
+    }
+    //Final Execution
+    else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
